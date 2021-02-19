726bf0f4d7525331a7c0c6eae631dbf98b51d243
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-02-19 14:35:49 -0300

    Labels

--- snippets/forms.py
+++ snippets/forms.py
@@ -13,3 +13,42 @@ class SnippetForm(forms.ModelForm):
     class Meta:
         model = Snippet
         fields = ['name', 'description', 'snippet', 'language', 'public']
+        labels = {
+            'name': 'Nombre del snippet',
+            'description': 'Descripción',
+            'snippet': 'Introduce el código',
+            'language': 'Introduce el lenguaje',
+            'public': 'Visibilidad',
+        }
+        widgets = {
+            'name': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese el nombre del snippet'
+                }
+            )
+            'description': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese una descripción'
+                }
+            )
+            'snippet': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Ingrese el código'
+                }
+            )
+            'language': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Lenguaje utilizado'
+                }
+            )
+            'public': forms.TextInput(
+                attrs = {
+                    'class':'form-control',
+                    'placeholder': 'Visibilidad'
+                }
+            )
+        }
